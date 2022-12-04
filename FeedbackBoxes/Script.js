a63fc6dad4b6da5d896fa366c545f0419ff96523
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2022-12-04 01:38:51 -0600

    commiting progress

--- FeedbackBoxes/Script.js
+++ FeedbackBoxes/Script.js
@@ -1,4 +1,46 @@
 
+const ratings = document.querySelectorAll('.rating')
+const sendBtn = document.querySelectorAll('#send')
+const panel = document.querySelectorAll('#panel')
+
+//event bubbling
+ratingsContainer.addEventListener('click', (e) => {
+	if(e.target.parentNode.classList.contains('rating') && e.target.nextElementSibling) {
+		removeActive()
+		e.target.parentNode.classList.add('active')
+		selectedRating = e.target.nextElementSibling.innerHTML
+	} else if(
+		e.target.parentNode.classList.contains('rating') &&
+		e.target.previousSibling &&
+		e.target.previousElementSibling.nodeName === 'IMG'
+	) {
+		removeActive()
+		e.target.parentNode.classList.add('active')
+		selectedRating = e.target.innerHTML
+	}
+
+})
+
+sendBtn.addEventListener('click', (e) => {
+	panel.innerHTML = `
+        <i class="fas fa-heart"></i>
+        <strong>Thank You!</strong>
+        <br>
+        <strong>Feedback: ${selectedRating}</strong>
+        <p>We'll use your feedback to improve our customer support</p>
+    `
+})
+
+function removeActive() {
+	for(let i = 0; i < ratings.length; i++) {
+		ratings[i].classList.remove('active')
+	}
+}
+
+
+
+
+
 
 
 
